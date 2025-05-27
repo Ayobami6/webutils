0f2a250c6e9d23e1bc8a3376d9d55eca0c628709
Ayobami6 <ayobamidele006@gmail.com>
2025-05-27 12:31:34 +0100

    Update JsonDecoder function to accept io.ReadCloser instead of io.Reader

--- webutils.go
+++ webutils.go
@@ -332,7 +332,7 @@ func ToBytes(v any) ([]byte, error) {
 	return b, nil
 }
 
-func JsonDecoder(v any, reader *io.Reader) error {
+func JsonDecoder(v any, reader *io.ReadCloser) error {
 	// just decode in memory
 	err := json.NewDecoder(*reader).Decode(v)
 	if err != nil {
